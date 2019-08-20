f87682d74f995b487865a06a285852dd7e1f0aff
PseudoMera <albinesteban@outlook.com>
2019-08-20 14:28:03 -0400

    Deleted useless comments

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def get_file_last_modified_date(file_name, file_path):
     '''
 
 
-'''Solve problem with date, the script is uploading files that should not be e.g. 19 august - 19 febreuary = 0 so it uploads'''
 def file_modification_day(file_name, file_path):
     decoded_file_path = os.fsdecode(file_path)
     modification_time = os.path.getmtime(f'{decoded_file_path}/{file_name}')
@@ -38,7 +37,6 @@ def file_modification_month(file_name, file_path):
     return int(str.strip(temp[23:25]))
 
 def get_all_files(files_path):
-    '''login_gdrive()'''
     directory = os.fsencode(files_path)
     for files in os.listdir(directory):
         filename = os.fsdecode(files)
@@ -46,12 +44,7 @@ def get_all_files(files_path):
         if filename.endswith(files_extensions) and abs(file_modification_day(filename, tmpDir) - int(datetime.datetime.today().strftime('%d'))) <= 5 and abs(file_modification_month(filename, tmpDir) - int(datetime.datetime.today().strftime('%m'))) == 0:
             drive_file_handler(f'{tmpDir}/{filename}')
 
-'''
-def login_gdrive():
-    g_login = GoogleAuth()
-    g_login.LocalWebserverAuth()
-    drive = GoogleDrive(g_login)
-'''
+
 def drive_file_handler(file_path):
     folder_id = '1LypqyXyvfa9kwGptCI0PJoLk15ziOgBo'
     file_drive = drive.CreateFile({"title":file_path[21:],"parents":[{"kind":"drive#fileLink", "id": folder_id}]})
